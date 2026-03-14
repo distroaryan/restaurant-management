fb355dd9f69e73c5a19eb64124d693c49041baa1
Aryan123-rgb <srivastavaaryanalc76@gmail.com>
2026-03-14 21:31:20 +0530

    added config package

---
+++ internal/config/config.go
@@ -0,0 +1,47 @@
+package config
+
+import (
+	"log"
+	"strings"
+
+	"github.com/knadh/koanf/providers/env"
+	"github.com/knadh/koanf/v2"
+)
+
+type Config struct {
+	Env string `koanf:"env"`
+	Port int `koanf:"port"`
+	MongoURI string `koanf:"mongo_uri"`
+}
+
+func Loand() *Config {
+	k := koanf.New(".")
+
+	err := k.Load(env.Provider("APP_", ".", func(s string) string {
+		return strings.ToLower(strings.TrimPrefix(s, "APP_"))
+	}), nil)
+
+	if err != nil {
+		log.Fatalf("Error loading config: %v", err)
+	}
+
+	var cfg Config
+	if err := k.Unmarshal("", &cfg); err != nil {
+		log.Fatalf("Error unmarshalling config: %v", err)
+	}
+
+	// Set some default values if none is provided
+	if cfg.Port == 0 {
+		cfg.Port = 8080
+	}
+
+	if cfg.Env == "" {
+		cfg.Env = "development"
+	}
+
+	if cfg.MongoURI == "" {
+		cfg.MongoURI = "mongodb://127.0.0.1:27017"
+	}
+
+	return &cfg 
+}
---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"os"
+)
+
+func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8000"
+	}
+	router := gin.New()
+	router.Use(gin.Logger())
+
+	router.Run(":" + port)
+
+}
