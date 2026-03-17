8a45d64f19f053f179a1293c6e17900f441b0340
Aryan123-rgb <srivastavaaryanalc76@gmail.com>
2026-03-17 17:56:50 +0530

    added table handler

---
+++ internal/handler/table_handler.go
@@ -0,0 +1,86 @@
+package handler
+
+import (
+	"net/http"
+
+	"github.com/distroaryan/restaurant-management/internal/repository"
+	"github.com/gin-gonic/gin"
+)
+
+type TableHandler struct {
+	tableRepo *repository.TableRepository
+}
+
+func NewTableRepositroy(tableRepo *repository.TableRepository) *TableHandler {
+	return &TableHandler{
+		tableRepo: tableRepo,
+	}
+}
+
+type seatRequest struct {
+	Seats int `json:"seats" binding:"required,min=1"`
+}
+
+func (h *TableHandler) GetAllTables(c *gin.Context) {
+	tables, err := h.tableRepo.GetAllTables(c.Request.Context())
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error": "Failed to fetch tables",
+		})
+		return
+	}
+	c.JSON(http.StatusOK, tables)
+}
+
+func (h *TableHandler) GetTableById(c *gin.Context) {
+	id := c.Param("tableId")
+
+	table, err := h.tableRepo.GetTableById(c.Request.Context(), id)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error": "Failed to fetch tables",
+		})
+		return
+	}
+	c.JSON(http.StatusOK, table)
+}
+
+func (h *TableHandler) BookSeats(c *gin.Context) {
+	tableId := c.Param("tableId")
+
+	var req seatRequest
+
+	if err := c.BindJSON(&req); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "seats must be a positive integer"})
+		return
+	}
+
+	err := h.tableRepo.BookSeats(c.Request.Context(), tableId, req.Seats)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error": "Failed to fetch tables",
+		})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{"success": true})
+}
+
+func (h *TableHandler) ReleaseSeats(c *gin.Context) {
+	tableId := c.Param("tableId")
+
+	var req seatRequest
+
+	if err := c.BindJSON(&req); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "seats must be a positive integer"})
+		return
+	}
+
+	err := h.tableRepo.ReleaseSeats(c.Request.Context(), tableId, req.Seats)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error": "Failed to fetch tables",
+		})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{"success": true})
+}
